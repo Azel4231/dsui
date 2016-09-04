f8b88a81f9c6461f2e5d6f1a7a3c8a45debc6dda
Azel4231 <Azel4231@users.noreply.github.com>
2016-09-04 17:20:35 +0200

    Spec+Tests

--- src/core.clj
+++ src/core.clj
@@ -17,18 +17,32 @@
   (doto (frame #(print "Exiting..."))
     (.setTitle title)))
 
+
+(defn primitive? [v]
+  ((complement coll?) v))
+(defn homogenous? [coll]
+  (or (empty? coll)
+      (every? #(= (keys %) (keys (first coll))) coll)))
+(defn primitive-map? [m]
+  (every? primitive? (vals m)))
+
 (def dsui-spec (s/map-of keyword? sequential?))
+(s/def ::dsui (s/or :t ::table :ts ::tabsheet :fo ::form))
+(s/def ::table (s/and (s/coll-of map?) (s/every primitive-map?) homogenous?))
+(s/def ::tabsheet (s/map-of any? ::dsui))
+(s/def ::form (s/map-of any? primitive?))
 
 (defn dsui
   "Creates a Swing ui for a nested data structure. Expects a Dereferable containing the data structure."
-  [d]
-  (let [conformed (s/conform dsui-spec @d)
+  [ds]
+  (let [conformed (s/conform dsui-spec ds)
         fr (setup-frame (str conformed))]
     
     ))
 
 
-(def a (atom {:a [:b]}))
-(dsui a)
+(def a {:t [{:a 1 :b 2} {:a "x" :b "y"}] :b {:some "other stuff"}})
+;(dsui b)
 
+(s/conform ::dsui a)
 
---
+++ test/core-test.clj
@@ -0,0 +1,23 @@
+(ns dsui.core-test
+  (:use dsui.core)
+  (:use clojure.test))
+
+
+
+(deftest primitive-test
+  (is (primitive? 1))
+  (is (not (primitive? []))))
+
+(def prmap {:a 1 :b "2" :c 1/2})
+(def nestmap {:a []})
+
+(deftest primitive-map-test
+  (is (primitive-map? prmap))
+  (is (not (primitive-map? nestmap))))
+
+
+(def homo '({:a 1 :b 2} {:a "1" :b "2"} {:a 1/3 :b 3/7}))
+(def inhomo  [{:a 1} {:b 2}])
+(deftest homo-test
+  (is (homogenous? homo)
+      (not (homogenous? inhomo))))

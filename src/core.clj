80feff243bb5f0ec8ffe608f03f7f7530928df4f
Azel4231 <Azel4231@users.noreply.github.com>
2016-09-06 17:25:43 +0200

    First working form with gridlayout

--- src/core.clj
+++ src/core.clj
@@ -1,7 +1,8 @@
 (ns dsui.core
   (:require [clojure.spec :as s])
-  (:import [javax.swing JComponent JFrame JLabel JPanel JTextField JRadioButton JTabbedPane JCheckBox JComboBox JScrollPane BoxLayout]
-           [java.awt.event KeyAdapter WindowAdapter]))
+  (:import [javax.swing JComponent JFrame JLabel JPanel JTextField JRadioButton JTabbedPane JCheckBox JComboBox JScrollPane SpringLayout]
+           [java.awt.event KeyAdapter WindowAdapter]
+           [java.awt GridLayout]))
 
 (set! *warn-on-reflection* true)
 
@@ -20,13 +21,27 @@
 (s/def ::form-spec (s/map-of any? (s/or ::field primitive? ::nested-ui ::dsui-spec)))
 
 
-; Map with settings. Define Spec with default values.
+; Todo Map with settings.
 (defn ^JFrame frame [close-f]
   (doto (new JFrame)
     (.pack)
     (.setSize (new java.awt.Dimension 400 700))
     (.addWindowListener (proxy [WindowAdapter] [] (windowClosing [e] close-f)))
     (.setVisible true)))
+
+(defn add [^JComponent component & children]
+  (doseq [c (flatten children)] (.add component c)))
+
+(defn panel []
+  (doto (new JPanel)
+    (.setLayout (new GridLayout 0 6 2 2))))
+
+(defn field [value]
+  (doto (new JTextField (str value))
+    (.setEditable false)))
+
+(defn label [l]
+  (new JLabel (str l)))
 
 (defn setup-frame [title]
   (doto (frame #(print "Exiting..."))
@@ -36,20 +51,24 @@
 
 (defmethod create ::named-tabsheet [this ds] (let [tpane (new JTabbedPane)]
     (->> ds
-         (map #(.addTab tpane (key %) (new JLabel (str (val %)))) )
+         (map #(.addTab tpane (key %) (label (val %))) )
          doall)))
 
 (defmethod create ::form [this ds]
-  (new JLabel (str ds)))
+  (let [p (panel)]
+    (->> ds
+         (mapcat (fn [[kw childDs]] [(label kw) (apply create childDs)]))
+         (add p))
+    p))
 
 (defmethod create ::field [this ds]
-  (new JLabel (str ds)))
+  (field ds))
 
 (defmethod create ::nested-ui [this [kw v]]
   (create kw v))
 
 (defn dsui
-  "Creates a Swing UI for a nested data structure."
+  "Creates a (write-only) Swing UI for a nested data structure."
   [ds]
   (let [^JFrame fr (setup-frame "DS UI")
         [kw v] (s/conform ::dsui-spec ds)]
@@ -60,9 +79,63 @@
 (def a {:t [{:a 1 :b 2} {:a "x" :b "y"}] :b {:some "other stuff"}})
 (def b [a {:c 23 :d 42}])
 (def c {:a [{:list "of stuff"}] :b 3})
-                                        ;(dsui b)
-(def ts {:a 1 :b "2"})
+(def ts {:a 1 :b "2" :c 42 :d 23 :? "argh"})
+(def mage {:id (. java.util.UUID randomUUID)
+   :name name
+   :player ""
+   :chronicle ""
+   :concept ""
+   :virtue ""
+   :vice ""
+   :size-natural 5
+   :properties {:path :obrimos
+                :order :mysterium
+                :cabal ""
+                :gnosis 1
+                :willpower 1
+                :wisdom 7
+                :merits #{}
+                :flaws #{}
+                :attributes {:strength 1 :dexterity 5 :stamina 1,
+                             :intelligence 1 :wits 1 :resolve 1,
+                             :presence 1 :manipulation 1 :composure 1}
+                :skills {:academics 0 :computer 0 :crafts 0 :investigation 0 :medicine 0 :occult 0 :politics 0 :science 0,
+                         :athletics 0 :brawl 0 :drive 0 :firearms 0 :larceny 0 :stealth 0 :survival 0 :weaponry 0,
+                         :animalKen 0 :empathy 0 :expression 0 :intimidation 0 :persuasion 0 :socialize 0 :streetwise 0 :subterfuge 0}
+                :arcana {:death 0
+                         :fate 0
+                         :forces 0
+                         :life 0
+                         :matter 0
+                         :mind 0
+                         :prime 0
+                         :space 0
+                         :spirit 0
+                         :time 0}
+                :rotes #{}
+                :waepons #{}
+                :experience-total 0
+                :experience-current 0}
+   :ledger {:bashing 0
+            :lethal 0
+            :aggravated 0
+            :mana 5
+            :size-current 5
+            :armor {:bashing 0
+                    :lethal 0
+                    :aggravated 0}
+            :spells-active 0
+            :spells-cast-upon-self 0
+            :spells-protective 0
+            :nimbus :none
+            :paradox-marks :none}
+   :historicProperties []})
+
 
 (dsui ts)
-(s/conform ::dsui-spec c)
+
+; primitives currently not supported
+#_(dsui "2")
+
+(s/conform ::dsui-spec ts)
 
